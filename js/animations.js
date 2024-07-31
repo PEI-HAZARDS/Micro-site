481e1cb9677db826485a59ee5619db23d8b4cf6c
Paul-Y5 <pauladascardoso@gmail.com>
2024-07-31 23:45:51 +0100

    debug

--- js/animations.js
+++ js/animations.js
@@ -1,8 +1,16 @@
 /* Animação Menu Mobile */
 let btnMenu = document.getElementById('btn-menu');
 let menuMobile = document.getElementById('btn-mobile');
+let overlay = document.getElementById("overlay-mobile");
 
 btnMenu.addEventListener('click', function() {
     menuMobile.classList.add('menu-mobile-ativo');
     btnMenu.classList.add('btn-menu-desligado');
+    overlay.classList.add('overlay-ativo');
+});
+
+menuMobile.addEventListener('click', function() {
+    menuMobile.classList.remove('menu-mobile-ativo');
+    btnMenu.classList.remove('btn-menu-desligado');
+    overlay.classList.remove('overlay-ativo');
 });
