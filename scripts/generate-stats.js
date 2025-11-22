fa8ec96a064e36887af4d5cce774c47dc3163936
paulo5 <paulo.c50@ua.pt>
2025-11-22 15:33:16 +0000

    ...

--- scripts/generate-stats.js
+++ scripts/generate-stats.js
@@ -14,12 +14,29 @@ async function getJson(url) {
   return res.json();
 }
 
+async function getCommitsCount() {
+  const url = `https://api.github.com/repos/${owner}/${repo}/commits?per_page=1`;
+  const res = await fetch(url, {
+    headers: {
+      Authorization: `Bearer ${token}`,
+      Accept: "application/vnd.github+json",
+    },
+  });
+  const json = await res.json();
+  const link = res.headers.get('link');
+  if (link) {
+    const lastPageMatch = link.match(/page=(\d+)>; rel="last"/);
+    if (lastPageMatch) {
+      return parseInt(lastPageMatch[1]);
+    }
+  }
+  return json.length; // fallback
+}
+
 (async () => {
   try {
-    // Último commit -> dá o total de commits pelo link header?
-    const commits = await getJson(
-      `https://api.github.com/repos/${owner}/${repo}/commits?per_page=1`
-    );
+    // Total de commits
+    const commits = await getCommitsCount();
 
     // Issues fechadas
     const issues = await getJson(
@@ -34,7 +51,7 @@ async function getJson(url) {
     // JSON final
     const stats = {
       updated_at: new Date().toISOString(),
-      commits: commits.length || 0,
+      commits: commits,
       issues_closed: issues.total_count || 0,
       releases: releases.length || 0,
     };
