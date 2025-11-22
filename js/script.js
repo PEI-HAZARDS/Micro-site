cf2085b9ff00fd000604c814ac5c47ac53e74af5
paulo5 <paulo.c50@ua.pt>
2025-11-22 15:50:56 +0000

    agora sim..

--- js/script.js
+++ js/script.js
@@ -381,7 +381,7 @@ function initDetailPageNavigation() {
 // New: fetch repository stats from GitHub and populate counters
 async function initRepoStats() {
   try {
-    const res = await fetch('/stats.json');
+    const res = await fetch('/Micro-site/stats.json');
     if (!res.ok) {
       throw new Error(`HTTP error! status: ${res.status}`);
     }
